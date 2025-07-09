bc84dfaecdb4813a93525ec47804f60790bbaa35
john-guioguio <190331326+john-guioguio@users.noreply.github.com>
2025-07-09 16:19:31 +0800

    fixed input not display

--- resources/js/Pages/Auth/AcceptInvitation.vue
+++ resources/js/Pages/Auth/AcceptInvitation.vue
@@ -42,21 +42,21 @@ const submit = () => {
             <input type="text" name="fake_username" autocomplete="username" style="display: none;" />
             <input type="password" name="fake_password" autocomplete="new-password" style="display: none;" />
 
-            <div v-if="props.user">
+            <div v-if="!props.user">
                 <InputLabel for="name" value="Your Name" />
                 <TextInput v-model="form.name" id="name" type="text" class="mt-1 block w-full " autocomplete="off"
                     placeholder="Complete Name" required autofocus />
                 <InputError :message="form.errors.name" class="mt-2" />
             </div>
 
-            <div v-if="props.user">
+            <div v-if="!props.user">
                 <InputLabel for="password" value="Password" />
                 <TextInput v-model="form.password" id="password" type="password" class="mt-1 block w-full " required
                     autocomplete="off" placeholder="Password" />
                 <InputError :message="form.errors.password" class="mt-2" />
             </div>
 
-            <div v-if="props.user">
+            <div v-if="!props.user">
                 <InputLabel for="password_confirmation" value="Confirm Password" />
                 <TextInput v-model="form.password_confirmation" id="password_confirmation" type="password"
                     autocomplete="new-name" placeholder="Confirm Password" class="mt-1 block w-full " required />
