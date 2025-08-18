0df1f0f2d2011ac04b02b4c274a8bcf5eb51c946
Jevguio <johnvincent@mata.ph>
2025-08-18 14:30:23 +0800

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -35,8 +35,9 @@ const refreshDashboardData = () => {
                 <TimeTracker @change="refreshDashboardData"></TimeTracker>
             </MainContainer>
 
-            <MainContainer
-                class="grid gap-5 sm:gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-3 sm:pt-5 pb-4 sm:pb-6 border-b dark:border-[#303F61]  items-stretch">
+            <MainContainer :class="canViewMembers()?
+            'grid gap-5 sm:gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-3 sm:pt-5 pb-4 sm:pb-6 border-b dark:border-[#303F61]  items-stretch':
+            'grid gap-5 sm:gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pt-3 sm:pt-5 pb-4 sm:pb-6 border-b dark:border-[#303F61]  items-stretch'" >
                 <RecentlyTrackedTasksCard></RecentlyTrackedTasksCard>
                 <LastSevenDaysCard></LastSevenDaysCard>
                 <ActivityGraphCard></ActivityGraphCard>
