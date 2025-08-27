e809020d6f1de030c026239064f03bad4998fa55
Jevguio <johnvincent@mata.ph>
2025-08-27 11:12:24 +0800

    size adjusted

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -46,7 +46,7 @@
 
 <body class="bg-[#0C101E]">
     <div class="min-w-[300px] w-[60%] mx-auto">
-        <img src="{{ asset('images/maintenance.png') }}" class="min-w-[100px] w-[25%] mx-auto p-3" />
+        <img src="{{ asset('images/maintenance.png') }}" class="min-w-[100px] w-[20%] mx-auto p-3" />
         <h1 class="text-center text-5xl font-bold py-5">We'll be back soon!</h1>
         <div class="text-center px-2">
             <p class="text-2xl text-center px-4 font-light">Sorry for the inconvenience but we're performing some
@@ -54,7 +54,7 @@
                 We'll be back online shortly!</p>
             {{-- <p>&mdash; The Team</p> --}}
         </div>
-        <img src="{{ asset('images/logo.png') }}" class="min-w-[100px] w-[40%] mx-auto " />
+        <img src="{{ asset('images/logo.png') }}" class="min-w-[100px] w-[45%] mx-auto " />
     </div>
 </body>
 
